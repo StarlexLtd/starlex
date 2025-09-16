f8c381064315bfdbf8655862fbb8950f6ac14021
Chris Liu <chris@ixvin.com>
2025-09-16 08:31:05 +0800

    改进装饰器，支持写入。

--- packages/core/src/patterns/decorator.ts
+++ packages/core/src/patterns/decorator.ts
@@ -7,13 +7,13 @@ export interface Decorator<T> {
 
 export abstract class Decorator<T> {
     constructor(public readonly target: T) {
-        assert.ok(target, `'target' is required.`);
+        assert(target, `'target' is required.`);
 
         //@ts-ignore
         this[PatchFlags.IsDecorator] = true;
     }
 
-    static Create<D extends Decorator<T>, T = D extends Decorator<infer T> ? T : never>(decorator: D): T & Decorator<T> {
+    static Create<D extends Decorator<T>, T = D extends Decorator<infer K> ? K : never>(decorator: D): T & Decorator<T> {
         return new Proxy<D>(decorator, {
             get(target: D, prop: string | symbol, receiver: any) {
                 if (prop in target) {
@@ -27,7 +27,20 @@ export abstract class Decorator<T> {
                     //@ts-ignore
                     return target.target[prop];
                 }
-            }
+            },
+            set(target: D, prop: string | symbol, newValue: any, receiver: any) {
+                if (prop in target) {
+                    return Reflect.set(target, prop, newValue, receiver);
+                } else if (prop === PatchFlags.IsProxy) {
+                    // PatchFlags 不能修改
+                    return false;
+                } else{
+                    // 装饰器上不存在的属性，直接写入被装饰的对象。
+                    //@ts-ignore
+                    target.target[prop] = newValue;
+                    return true;
+                }
+            },
         }) as any as T & Decorator<T>;
     }
 }
--- packages/core/src/utils/index.ts
+++ packages/core/src/utils/index.ts
@@ -1,2 +1,10 @@
-export * from "./assert";
+/**
+ * Assert
+ * @param condition Condition to test.
+ * @param message If condition test fails, throw this message as Error.
+ */
+export function assert(condition: any, message?: string) {
+    if (!!!condition) throw new Error(message)
+}
+
 export * from "./common";
