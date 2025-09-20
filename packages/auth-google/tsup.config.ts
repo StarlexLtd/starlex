424953670adfc0c0e4dec6e171ff2d535e470c3d
Chris Liu <chris@ixvin.com>
2025-09-20 16:00:06 +0800

    改进 tsup 配置。

--- packages/auth-google/tsup.config.ts
+++ packages/auth-google/tsup.config.ts
@@ -1,18 +1,6 @@
 import { defineConfig } from "tsup";
+import rootConfig from "../../tsup.config.root";
 
 export default defineConfig({
-    entry: ["src/**/*.{ts,js}"],
-    outDir: "dist",
-    format: ["esm"],
-    sourcemap: true,
-    clean: true, // clean `outDir` before build.
-    tsconfig: "tsconfig.json", // spec tsconfig
-    dts: {
-        entry: ["src/index.ts"],
-        // outputDir: 'dist/src',
-        only: false,
-    },
-    splitting: false, // 禁用代码分割以避免生成 chunk-xxxx 文件
-    keepNames: true,  // 保留原始文件名
-    treeshake: false, // 禁用 tree-shaking 以确保输出文件与输入文件一一对应
+    ...rootConfig,
 });
---
+++ tsup.config.root.ts
@@ -0,0 +1,17 @@
+import { defineConfig } from "tsup";
+
+export default defineConfig({
+    entry: ["src/**/*.{ts,js}"],
+    outDir: "dist",
+    format: ["esm"],
+    sourcemap: true,
+    clean: true, // clean `outDir` before build.
+    tsconfig: "tsconfig.json", // spec tsconfig
+    dts: {
+        entry: ["src/index.ts"],
+        only: false,
+    },
+    splitting: false, // 禁用代码分割以避免生成 chunk-xxxx 文件
+    keepNames: true,  // 保留原始文件名
+    treeshake: false, // 禁用 tree-shaking 以确保输出文件与输入文件一一对应
+});
