712cff09371acbb824cf2ef88ca8b3caebda8d13
Chris Liu <chris@ixvin.com>
2025-09-22 14:32:58 +0800

    改进 tsup 配置。

--- tsup.config.root.ts
+++ tsup.config.root.ts
@@ -11,7 +11,8 @@ export default defineConfig({
         entry: ["src/index.ts"],
         only: false,
     },
-    splitting: false, // 禁用代码分割以避免生成 chunk-xxxx 文件
-    keepNames: true,  // 保留原始文件名
-    treeshake: false, // 禁用 tree-shaking 以确保输出文件与输入文件一一对应
+    splitting: false,   // 禁用代码分割以避免生成 chunk-xxxx 文件
+    keepNames: true,    // 保留原始文件名
+    treeshake: true,    // 禁用 tree-shaking 以确保输出文件与输入文件一一对应
+    bundle: false,      // 禁用 bundling，保持原始 export 语句
 });
