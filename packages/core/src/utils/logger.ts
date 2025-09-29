6556e4958b935bc43cf605117936653fc573a3c7
Chris Liu <chris@ixvin.com>
2025-09-29 21:22:55 +0800

    core: 优化日志函数的彩色前缀。

--- packages/core/src/utils/logger.ts
+++ packages/core/src/utils/logger.ts
@@ -11,14 +11,20 @@ const getStatus = (key: "DEBUG" | "TRACE"): boolean =>
 const __DEBUG = getStatus("DEBUG");
 const __TRACE = getStatus("TRACE");
 
-const chalkText = IS_BROWSER ? chalk.whiteBright : chalk.black;
+const chalkText = chalk.whiteBright;
+const successPrefix = chalkText.bgHex(IS_BROWSER ? "#14A44D" : "#15803D")(" SUCCESS ");
+const errorPrefix   = chalkText.bgHex(IS_BROWSER ? "#DC4C64" : "#DC2626")("  ERROR  ");
+const warnPrefix    = chalkText.bgHex(IS_BROWSER ? "#E4A11B" : "#B45309")(" WARNING ");
+const infoPrefix    = chalkText.bgHex(IS_BROWSER ? "#54B4D3" : "#0E7490")("  INFO   ");
+const debugPrefix   = chalkText.bgHex(IS_BROWSER ? "#3B71CA" : "#2563EB")("   LOG   ");
+const tracePrefix   = chalkText.bgHex(IS_BROWSER ? "#9FA6B2" : "#6B7280")("  TRACE  ");
 
-const success = console.log.bind(console, chalkText.bgGreenBright(" SUCCESS "));
-const error = console.error.bind(console, chalkText.bgRedBright(IS_BROWSER ? " ERROR " : "  ERROR  "));
-const warn = console.warn.bind(console, chalkText.bgYellow(" WARNING "));
-const info = console.info.bind(console, chalkText.bgCyan(IS_BROWSER ? " INFO " : "  INFO   "));
-const debug = __DEBUG ? console.log.bind(console, chalkText.bgBlueBright(IS_BROWSER ? " LOG " : "   LOG   ")) : () => { };
-const trace = __TRACE ? console.debug.bind(console, chalkText.bgGray(IS_BROWSER ? " TRACE " : "  TRACE  ")) : () => { };
+const success = console.log.bind(console, successPrefix);
+const error   = console.error.bind(console, errorPrefix);
+const warn    = console.warn.bind(console, warnPrefix);
+const info    = console.info.bind(console, infoPrefix);
+const debug   = __DEBUG ? console.log.bind(console, debugPrefix) : () => { };
+const trace   = __TRACE ? console.debug.bind(console, tracePrefix) : () => { };
 
 const _log = debug as any;
 _log.success = success;
